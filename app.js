b987ad685cc08e68cf978d2fb38777109f3f738f
전선일 <jeonsunil1225@gmail.com>
2025-03-04 12:27:52 +0900

    베이직 형태의 handlebars 사용해보기

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const express = require('express');
+const { engine } = require('express-handlebars');
+
 const app = express();
 
-const path = require('path');
-app.use(express.static(path.join(__dirname, 'public')));
-// ? 일단 사용한다는 뜻의 파일 리딩
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
 
-// app.get("/", (req, res) => {
-//   let test = path.join(__dirname, 'public', 'index.html')
-//   res.sendFile(test);
-//   // ? 파일 가져오기
-
-// });
+app.get('/', (req, res) => {
+    res.render('home');
+});
 
 app.listen(3000, () => {
   console.log('http://localhost:3000/')
-})
+});
