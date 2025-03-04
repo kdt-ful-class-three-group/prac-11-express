075ff6e6447020a919dc738a419dbb97fd69ef30
전선일 <jeonsunil1225@gmail.com>
2025-03-04 12:57:02 +0900

    코드를 쓰다보니 생기는 궁금증을 주석으로 달았음

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { engine } = require('express-handlebars');
+// ? engine은 왜 {}로 감싸야 기능을 하는 것이지?
 
 const app = express();
 
 app.engine('handlebars', engine());
+// ? handlebars를 함수로 쓰고 있는건가?
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
