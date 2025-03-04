e5a3325cf34fc46a5310f7c3fa66edd4676beb2e
전선일 <jeonsunil1225@gmail.com>
2025-03-04 12:08:53 +0900

    핸들바를 사용하도록 노력해보는 중

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 
 const path = require('path');
+const Handlebars = require("handlebars");
+const template = Handlebars.compile("Name: {{name}}");
+console.log(template({ name: "Nils" }));
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 // ? 일단 사용한다는 뜻의 파일 리딩
 
