7da04cb4dfa2633f3da016160710ec788d2fc19d
전선일 <jeonsunil1225@gmail.com>
2025-03-04 11:51:24 +0900

    app.use 사용

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send('헬로월드')
-});
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+// ? 일단 사용한다는 뜻의 파일 리딩
+
+// app.get("/", (req, res) => {
+//   let test = path.join(__dirname, 'public', 'index.html')
+//   res.sendFile(test);
+//   // ? 파일 가져오기
+
+// });
 
 app.listen(3000, () => {
   console.log('http://localhost:3000/')
